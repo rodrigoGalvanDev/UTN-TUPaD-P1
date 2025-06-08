098b080eff18600a6d9d2ed875957fd2a6877dbc
Rodrigo Galvan <97800474+rodrigoGalvanDev@users.noreply.github.com>
2025-06-08 20:54:19 -0300

    changing the method used on this project.py

--- TIP/menu_interactivo.py
+++ TIP/menu_interactivo.py
@@ -1,61 +1,56 @@
-class Nodos:
-    def __init__(self, name, action=None):
-        self.name = name                  # Nombre del menú u opción
-        self.action = action              # Función a ejecutar (si es hoja)
-        self.children = []                # Subopciones
-        self.parent = None                # Para retroceder
+import time
 
-    def add_child(self, child_node):
-        ##child_node.parent = self
-        self.children.append(child_node)
+arbol = ["Menu Principal", ["1_ Consulta de saldo", ["1_ Consultar saldo cuenta corriente"], ["2_ Consultar saldo cuenta ahorro"]], ["2_ Retiro de efectivo", ["1_ Retirar 20 mil pesos"], ["2_ Retirar un monto especifico"]], ["3_ Depositos",["1_ Depositar en efectivo",], ["2_ Depositar en sobre"]], ["4_ Transferencias", ["1_ Transferir a un alias"], ["2_ Transferir a un CBU/CVU"]], ["5_ Cambiar de clave", ["1_Cambiar clave del cajero"], ["2_Cambiar clave del home banking"]]]
 
-    def is_leaf(self):
-        return len(self.children) == 0
-
-
-menu_principal = Nodos("Menu Principal")
-submenu_consulta = Nodos("1_ Consulta de saldo")
-submenu_retiro = Nodos("2_ Retiro de efectivo")
-submenu_depositos = Nodos("3_ Depositos")
-submenu_transferencias = Nodos("4_ Transferencias")
-submenu_cambiar_clave = Nodos("5_ Cambiar de clave")
-
-opcion_consulta_cc = Nodos("1_ Consultar saldo cuenta corriente", 'Su saldo es de 133928 pesos')
-opcion_consulta_ca = Nodos("2_ Consultar saldo cuenta ahorro", 'Su saldo es de 900 pesos')
-opcion_retiro_20 = Nodos("1_ Retirar 20 mil pesos", "Se ha retirado con exito 20 mil pesos")
-opcion_retiro_monto = Nodos("2_ Retirar un monto en especifico", "Ingrese el monto a retirar: ")
-opcion_deposito_efectivo = Nodos("1_ Depositar en efectivo", "Ingrese los billetes por el cajero por favor.")
-opcion_deposito_sobre = Nodos("2_ Depositar en sobre", "Ingrese el sobre por el cajero por favor.")
-opcion_transferencia_alias = Nodos("1_ Transferir a un alias", "Ingrese el alias: ")
-opcion_transferencia_CBU = Nodos("2_ Transferir a un CBU/CVU", "Ingrese el CBU/CVU: ")
-opcion_cambiar_clave_cajero = Nodos("1_Cambiar clave del cajero automatico", "Ingrese la nueva clave:")
-opcion_cambiar_clave_hb = Nodos("2_Cambiar clave del home banking", "Ingrese la nueva clave del home banking: ")
-
-menu_principal.add_child(submenu_consulta)
-menu_principal.add_child(submenu_retiro)
-menu_principal.add_child(submenu_depositos)
-menu_principal.add_child(submenu_transferencias)
-menu_principal.add_child(submenu_cambiar_clave)
-
-submenu_consulta.add_child(opcion_consulta_cc)
-submenu_consulta.add_child(opcion_consulta_ca)
-
-submenu_retiro.add_child(opcion_retiro_20)
-submenu_retiro.add_child(opcion_retiro_monto)
-
-submenu_depositos.add_child(opcion_deposito_efectivo)
-submenu_depositos.add_child(opcion_deposito_sobre)
-
-
-
-print(f'Bienvenido al banco:\n {menu_principal.children[0].name} \n {menu_principal.children[1].name} \n {menu_principal.children[2].name} \n {menu_principal.children[3].name} \n {menu_principal.children[4].name}')
-
-eleccion = input("Por favor, elige una opcion:")
+print("Bienvenido al banco")
+for submenu in arbol[1:]:
+    print(submenu[0])
+eleccion = input("Por favor, seleccione una opcion: ")
 
 if(eleccion == "1"):
-    eleccion = input(f'{submenu_consulta.children[0].name}\n{submenu_consulta.children[1].name}\nPor favor, elige una opcion: ')
-    if(eleccion == "1"):
-        print(opcion_consulta_cc.action)
-
-
+    print(f"{arbol[1][1][0]}\n{arbol[1][2][0]}")
+    eleccion = input("Por favor, seleccione una opcion: ")
+    if (eleccion == "1"):
+        print("Su saldo es de 13337 pesos")
+    else:
+        print("Su saldo es de 100 pesos")
+elif(eleccion == "2"):
+    print(f"{arbol[2][1][0]}\n{arbol[2][2][0]}")
+    eleccion = input("Por favor, seleccione una opcion: ")
+    if (eleccion == "1"):
+        print("Usted retiro 20 mil pesos.")
+    else:
+        monto = input("Ingrese el monto a retirar: ")
+        print(f"Usted retiro {monto} pesos de manera exitosa.")
+elif(eleccion == "3"):
+    print(f"{arbol[3][1][0]}\n{arbol[3][2][0]}")
+    eleccion = input("Por favor, seleccione una opcion: ")
+    if (eleccion == "1"):
+        print("Ingrese el dinero por la boquilla del cajero")
+        time.sleep(3)
+        print("Se ingreso el dinero de manera exitosa")
+    else:
+        print("Ingrese el sobre por la boquilla del cajero")
+        time.sleep(3)
+        print("Se ingreso el dinero de manera exitosa")
+elif(eleccion == "4"):
+    print(f"{arbol[4][1][0]}\n{arbol[4][2][0]}")
+    eleccion = input("Por favor, seleccione una opcion: ")
+    if (eleccion == "1"):
+        alias = input("Ingrese el alias: ")
+        monto = input("Ahora ingrese el dinero a transferir: ")
+        print(f"Se ha realizado la transferncia con exito al alias: {alias} con un monto total de {monto} pesos")
+    else:
+        cbu = input("Ingrese el alias: ")
+        monto = input("Ahora ingrese el dinero a transferir: ")
+        print(f"Se ha realizado la transferncia con exito al cbu: {cbu} con un monto total de {monto} pesos")
+elif(eleccion == "5"):
+    print(f"{arbol[5][1][0]}\n{arbol[5][2][0]}")
+    eleccion = input("Por favor, seleccione una opcion: ")
+    if (eleccion == "1"):
+        input("Ingrese la nueva contraseña para usar el cajero: ")
+        print("La contraseña se cambio con exito")
+    else:
+        input("Ingrese la nueva contraseña para usar su home banking: ")
+        print("La contraseña se cambio con exito")
 
